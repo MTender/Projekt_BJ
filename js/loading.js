090aa124ecabe30b4cb6bcac97098334d61cef17
Märt Tender <mart.tender@gmail.com>
2020-11-21 19:42:08 +0200

    Javascript loading icon implementation

---
+++ js/loading.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function(){
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden";
+    const overlay = document.querySelector(".overlay");
+    overlay.className += " hidden";
+});
